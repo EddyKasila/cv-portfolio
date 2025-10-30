327d4669a116e2430cae763481f9ab331ce98a0b
EddyKasila <eddynguli@gmail.com>
2025-10-30 18:53:35 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Use the BASE_URL environment variable, defaulting to '/' for local development
   base: process.env.BASE_URL || '/',
-  site: 'https://EddyKasila.github.io',
+  site: 'https://EddyKasila.github.io/cv-portfolio',
 });
